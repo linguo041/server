a696698a35827ea2fc8214d07fade78a7a39abb3
znzhang1985 <znzhang1985@163.com>
2016-10-02 17:46:15 +0800

    update aliyun url

--- duoshouji-image/src/main/java/com/duoshouji/server/internal/image/AliyunOssImageStore.java
+++ duoshouji-image/src/main/java/com/duoshouji/server/internal/image/AliyunOssImageStore.java
@@ -18,14 +18,16 @@ import com.duoshouji.util.MobileNumber;
 public class AliyunOssImageStore implements ImageStore {
 	private static final String ACCESS_KEY_ID = "REQ4WWCYXaNWqTBZ";
 	private static final String ACCESS_KEY_SECRET = "aC1RtnoiQ4xj96R7U4nFfJPxO9tSeT";
-	private static final String END_POINT = "images.share68.com";
+	private static final String ALIYUN_END_POINT = "oss-cn-shanghai.aliyuncs.com";
 	private static final String BUCKET_NAME = "duoshouji-test";
+	
+	private static final String DUOSHOUJI_END_POINT = "images.share68.com";
 	
 	private OSSClient client;
 	
 	@PostConstruct
 	public void init() {
-		client = new OSSClient(END_POINT, ACCESS_KEY_ID, ACCESS_KEY_SECRET);
+		client = new OSSClient(ALIYUN_END_POINT, ACCESS_KEY_ID, ACCESS_KEY_SECRET);
 	}
 	
 	@PreDestroy
@@ -57,9 +59,7 @@ public class AliyunOssImageStore implements ImageStore {
 	
 	private URL buildObjectURL(String key) throws MalformedURLException {
 		StringBuilder urlBuilder = new StringBuilder("http://");
-		urlBuilder.append(BUCKET_NAME);
-		urlBuilder.append('.');
-		urlBuilder.append(END_POINT);
+		urlBuilder.append(DUOSHOUJI_END_POINT);
 		urlBuilder.append('/');
 		urlBuilder.append(key);
 		return new URL(urlBuilder.toString());
